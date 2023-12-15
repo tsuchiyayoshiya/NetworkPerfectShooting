b72877ffb34c2e865b620763d595b62c595fc558
優吾 中林 <220159@jc-21.jp>
2023-12-15 10:10:48 +0900

    てえええええええええい

---
+++ Socket.cpp
@@ -0,0 +1,46 @@
+#include "Socket.h"
+
+bool Socket::Init()
+{
+	return WSAGetLastError();
+}
+
+bool Socket::InitSocket(int sockType)
+{
+	return WSAGetLastError();
+}
+
+bool Socket::Connect(std::string serverIpv4Address, unsigned short port)
+{
+	return WSAGetLastError();
+}
+
+bool Socket::Send(SendElement _elem)
+{
+	
+	return WSAGetLastError();
+}
+
+bool Socket::Recv(SendElement* _elem)
+{
+	SendElement recvElem;
+	int ret;
+
+	// 受信
+	ret = recv(sock, (char*)&recvElem, sizeof(recvElem), 0);
+	// 失敗
+	if (ret != sizeof(recvElem))
+	{
+		return false;
+	}
+
+	// 成功時は受信データをバイトオーダーに変換
+	*_elem = ntohl(recvElem);
+	return true;
+	return WSAGetLastError();
+}
+
+bool Socket::Exit()
+{
+	return WSAGetLastError();
+}
---
+++ Socket.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdlib>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include "Engine/Transform.h"
+
+#pragma comment( lib, "ws2_32.lib")
+
+const unsigned short SERVERPORT = 8888;
+
+const unsigned int MESSAGELENGTH = 1024;
+
+struct SendElement
+{
+	Transform playerPos;
+	std::vector<Transform> bulletPos;
+};
+
+class Socket
+{
+	int sock;
+public:
+	bool Init();
+	bool InitSocket(int sockType);
+	bool Connect(std::string serverIpv4Address, unsigned short port);
+	bool Send(SendElement _elem);
+	bool Recv(SendElement *_elem);
+	bool Exit();
+};
+
